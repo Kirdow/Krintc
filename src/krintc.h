e9d9a22ac7d531c4d2aef7bfabc0fe52c1354a1b
Kirdow <10155603+Kirdow@users.noreply.github.com>
2023-04-04 18:08:46 +0200

    add doc comments to 2 files
    add doc to krintc.h
    add doc to ptrlist.h

--- src/krintc.h
+++ src/krintc.h
@@ -5,23 +5,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * add alpha to a 24-bit BGR pixel
+ * note: turns 0xBBGGRR into 0xAABBGGRR with a fixed alpha of 0xFF
+ */
 #define ACOLOR(color) ((color) | 0xFF << (3*8));
+
+/*
+ * generic macros used for mathematical operations
+ * macro KRINTC_SIGN - copy the sign of a unit
+ * macro KRINTC_ABS - force a positive (x >= 0) value
+ * macro KRINTC_SWAP - swap two values
+ */
 #define KRINTC_SIGN(T, x) ((T)((x) > 0) - (T)((x) < 0))
 #define KRINTC_ABS(T, x) (KRINTC_SIGN(T, x) * (x))
 #define KRINTC_SWAP(T, x, y) do { T _tmp = x; x = y; y = _tmp; } while (0)
-#define NO_IMPL(x) do { printf("WARN: Not Implemented! {%s}\n", x); } while (0)
-#define RETURN_NO_IMPL(x) do { NO_IMPL(); return (x); } while (0)
 
+/*
+ * debug print for not yet implemented branches
+ * param x - required text parameter for extra data
+ */
+#define NO_IMPL(x) do { printf("WARN: Not Implemented! {%s}\n", x); } while (0)
+
+/*
+ * debug print with return for not yet implemented branches
+ * param x - value to return after printing
+ */
+#define RETURN_NO_IMPL(x) do { NO_IMPL(""); return (x); } while (0)
+
+/*
+ * clear the entire buffer with one color
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param color - color to fill
+ */
 void krintc_fill(u32 *pixels, uSize pixel_width, uSize pixel_height, u32 color);
+
+/*
+ * fill a rect inside the buffer with a specific color
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param x0, y0 - minimum rect point
+ * param x1, y1 - maximum rect point
+ * param color - color to fill
+ */
 void krintc_fill_rect(u32 *pixels, uSize pixel_width, uSize pixel_height, i32 x0, i32 y0, i32 x1, i32 y1, u32 color);
+
+/*
+ * fill a circle inside the buffer with a specific color
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param xc, yc - circle centre point
+ * param radius - circle radius (in pixels)
+ * param color - color to fill
+ */
 void krintc_fill_circle(u32 *pixels, uSize pixel_width, uSize pixel_height, i32 xc, i32 yc, i32 radius, u32 color);
+
+/*
+ * draw a line inside the buffer with a specific color
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param x0, y0 - line start point
+ * param x1, y1 - line end point
+ * param color - color to draw
+ */
 void krintc_line(u32 *pixels, uSize pixel_width, uSize pixel_height, i32 x0, i32 y0, i32 x1, i32 y1, u32 color);
+
+/*
+ * plot a single pixel inside the buffer with a specific color
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param x0, y0 - pixel point
+ * param color - color to plot
+ */
 void krintc_plot(u32 *pixels, uSize pixel_width, uSize pixel_height, i32 x0, i32 y0, u32 color);
+
+/*
+ * split a pixel into individual channels
+ * param pixel - input pixel in 0xBBGGRR format
+ * param red, green, blue - output channels
+ */
 void krintc_explode_rgb(u32 pixel, u32 *red, u32 *green, u32 *blue);
+
+/*
+ * combine pixel channels into a single pixel
+ * param pixel - output pixel in 0xBBGGRR format
+ * param red, green, blue - input channels in 0x00 - 0xFF format
+ */
 void krintc_implode_rgb(u32 *pixel, u32 red, u32 green, u32 blue);
+
+/*
+ * allocate a buffer of pixels compatible with Krint.c
+ * param pixels - output pixel buffer in 0xAABBGGRR format
+ * param pixel_width, pixel_height - input buffer dimensions
+ */
 uBool krintc_alloc_data(u32 **pixels, uSize pixel_width, uSize pixel_height);
+
+/*
+ * free up a buffer allocated using `krintc_alloc_data`
+ * param pixels - pixel buffer to free up
+ */
 uBool krintc_free_data(u32 **pixels);
 
+/*
+ * save image to disk to a specified path
+ * param pixels, pixel_width, pixel_height - buffer definition
+ * param filename - target filepath for image .png file
+ */
 uBool krintc_save_disk_image(const u32 *pixels, uSize pixel_width, uSize pixel_height, const c8 *filename);
 
 #endif // _KRINT_C_
--- src/ptrlist.h
+++ src/ptrlist.h
@@ -5,25 +5,114 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/*
+ * ptrlist_t implementation of vector list to store list of pointers
+ * 
+ * note: ptrlist_t does not take ownership of any pointers stored in,
+ *  the list. Upon using `ptrlist_remove_element` or `ptrlist_pop_element`
+ *  it is up to the caller to free up the memory help by the returned
+ *  pointer.
+ */
 typedef struct {
     void **data;
     size_t len;
 } ptrlist_t;
 
+/*
+ * create new ptrlist_t instance
+ * returns ptrlist_t pointer instance
+ */
 ptrlist_t *ptrlist_create();
+
+/*
+ * create new ptrlist_t with a pre-allocated size
+ * returns ptrlist_t pointer instance
+ * param count - pre-allocated item count
+ */
 ptrlist_t *ptrlist_create_count(size_t count);
+
+/*
+ * delete ptrlist_t instance
+ * param ptr - pointer to ptrlist_t pointer instance
+ */
 void ptrlist_free(ptrlist_t **ptr);
+
+/*
+ * internal: allocate storage of size `count`
+ * param ptr - ptrlist_t instance pointer
+ * param count - size of list to allocate
+ */
 bool ptrlist_alloc_count(ptrlist_t *ptr, size_t count);
+
+/*
+ * internal: reallocate storage of size `count`
+ * param ptr - ptrlist_t instance pointer
+ * param count - new size of list to reallocate
+ */
 bool ptrlist_realloc_count(ptrlist_t *ptr, size_t count);
+
+/*
+ * get pointer element at index in list
+ * returns pointer stored at index or NULL if index out of bounds
+ * param ptr - ptrlist_t instance pointer
+ * param index - index in the list
+ */
 void *ptrlist_get_element(const ptrlist_t *ptr, size_t index);
+
+/*
+ * set pointer element at index in list
+ * returns bool for success state, false if index out of bounds
+ * param ptr - ptrlist_t instance pointer
+ * param index - index in the list
+ * param value - pointer to store at index
+ */
 bool ptrlist_set_element(ptrlist_t *ptr, size_t index, void *value);
+
+/*
+ * add pointer element at end of list
+ * returns bool for success state
+ * param ptr - ptrlist_t instance pointer
+ * param value - pointer to store
+ */
 bool ptrlist_add_element(ptrlist_t *ptr, void *value);
+
+/*
+ * insert pointer element at index in list
+ * returns bool for success state, false if index out of bounds
+ * param ptr - ptrlist_t instance pointer
+ * param index - index in the list (note: insertion done before this index)
+ * param value - pointer to store
+ */
 bool ptrlist_insert_element(ptrlist_t *ptr, size_t index, void *value);
+
+/*
+ * remove pointer element at index in list
+ * returns removed pointer element (note: it's up to caller to free up memory)
+ * param ptr - ptrlist_t instance pointer
+ * param index - index in the list
+ */
 void *ptrlist_remove_element(ptrlist_t *ptr, size_t index);
+
+/*
+ * remove last pointer element in list
+ * returns removed pointer element (note: it's up to caller to free up memory)
+ * param ptr - ptrlist_t instance pointer
+ */
 void *ptrlist_pop_element(ptrlist_t *ptr);
 
+/*
+ * wrapper for `ptrlist_get_element` to cast to a type when returned
+ */
 #define ptrlist_get_type(T, ptr, index) ((T)(ptrlist_get_element(ptr, index)))
+
+/*
+ * wrapper for `ptrlist_remove_element` to cast to a type when returned
+ */
 #define ptrlist_remove_type(T, ptr, index) ((T)(ptrlist_remove_element(ptr, index)))
+
+/*
+ * wrapper for `ptrlist_pop_element` to cast to a type when returned
+ */
 #define ptrlist_pop_type(T, ptr) ((T)(ptrlist_pop_element(ptr)))
 
 #endif // _PTRLIST_H_
